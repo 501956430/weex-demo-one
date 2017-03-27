dc7e19b8e976b913530cc47cf9c82ed60125198d
tangbo <765471607@qq.com>
2017-03-27 16:57:19 +0800

    WS-2016.2.4 <tangbo@PC-JHH-IT-12 Create IntelliLang.xml, ide.general.xml, git.xml, abbreviations.xml, Default.xml, project.default.xml, diff.xml, laf.xml, other.xml, Weex File.we, ui.lnf.xml, web-browsers.xml, filetypes.xml, keymap.xml, vcs.xml, colors.scheme.xml, Vue File.vue, Darcula copy.icls, github_settings.xml, debugger.xml

---
+++ fileTemplates/Vue File.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script>
+    export default {
+        data (){
+          return {
+          
+          }
+        }
+    }
+</script>
+
+<style>
+
+</style>
